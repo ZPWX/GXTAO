64548162c0d254bc20e88830bbec5b034dd3809a
GXTAO <3157188714@qq.com>
2018-02-04 23:12:51 +0800

    InputStream

--- TestOutputStream.java
+++ TestOutputStream.java
@@ -1,7 +1,14 @@
+//利用输入流向文件中写数据
+//data:2.4.2018
+//author:GXTAO
 import java.io.*;
 public class TestOutputStream{
     public static void main(String[] args)throws Exception{
-    OutputStream os=new FileOutputStream("1.txt",true);//true,是否追加
-    os.write('A');
+        String str="Hello World!";
+        OutputStream os=new FileOutputStream("1.txt",true);//true,是否追加
+        byte[] bs=str.getBytes();//把字符串转成数组
+        os.write('A');
+        os.close();//关闭数据流，释放资源
     }
 }
+
