a865d3b3be99de3a2d30e7cc872cd71599ef891e
GXTAO <3157188714@qq.com>
2018-02-09 00:55:53 +0800

    服务器为多个客户端同时提供服务--多线程

--- 网络编程/TcpServer1.java
+++ 网络编程/TcpServer1.java
@@ -6,13 +6,16 @@ public class TcpServer1{
         ServerSocket ss=new ServerSocket(9000);
         //ServerSocket
         Socket s=ss.accept();//监听 阻塞方法 等待客户端的连接请求
-    OutputStream os =s.getOutputStream();
-    PrintWriter out=new PrintWriter(os);
-    for(int i=1;i<30;i++){
-        out.println("hello"+i);
-        out.flush();
-        Thread.sleep(500);
-    }
-    s.close();//关socket
+
+        //System.out.prinltn(s.getInetAddress());
+
+        OutputStream os =s.getOutputStream();
+        PrintWriter out=new PrintWriter(os);
+        for(int i=1;i<30;i++){
+                out.println("hello"+i);
+                out.flush();
+                Thread.sleep(500);
+            }
+        s.close();//关socket
     }
 }
